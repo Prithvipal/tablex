5f87489c7308c5351efa438df7192420def147ea
Prithvipal <prithvirathore99@gmail.com>
2022-10-23 22:21:14 +0530

    Initial code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Prithvipal/tablex
+
+go 1.18
---
+++ tablex.go
@@ -0,0 +1,64 @@
+package tablex
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	PADDING = 3
+)
+
+type Tablex struct {
+	columns []string
+	width   []int
+	rows    [][]any
+}
+
+func NewTablex(columns []string) *Tablex {
+	t := &Tablex{
+		columns: columns,
+		width:   make([]int, len(columns)),
+	}
+	for idx, col := range columns {
+		t.width[idx] = len(col) + PADDING
+	}
+
+	return t
+}
+
+func (t *Tablex) AddRow(row []any) {
+	for idx, ele := range row {
+		eleS := ele.(string)
+		if len(string(eleS))+PADDING > t.width[idx] {
+			t.width[idx] = len(string(eleS)) + 3
+		}
+	}
+	t.rows = append(t.rows, row)
+}
+
+func (t *Tablex) Draw() {
+	s := strings.Builder{}
+	s.WriteString("+")
+	for idx := range t.columns {
+		s.WriteString(strings.Repeat("-", t.width[idx]))
+		s.WriteString("+")
+	}
+	s.WriteString("\n")
+	for idx, col := range t.columns {
+		s.WriteString("|")
+		s.WriteString(" ")
+		s.WriteString(col)
+		s.WriteString(strings.Repeat(" ", t.width[idx]-len(col)-1))
+	}
+	s.WriteString("|")
+	s.WriteString("\n")
+	s.WriteString("+")
+	for idx := range t.columns {
+		s.WriteString(strings.Repeat("=", t.width[idx]))
+		s.WriteString("+")
+	}
+
+	fmt.Println(s.String())
+
+}
